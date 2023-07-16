5af6196eff9fc684fc75cd57390fe17835253d7e
Neha Christma <69244702+nehachristma@users.noreply.github.com>
2023-07-16 16:40:39 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+
+public class project {
+	public static void main(String[] args){
+		
+	student st=new student();
+	//st.createDatabase();
+	//st.createTable();
+	//CRUD
+	//create data
+	//st.createData();
+	st.readData();
+	//st.UpdateData();
+	//st.deleteData();
+}
+}
+
